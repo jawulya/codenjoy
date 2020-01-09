958fed8c7989de2a3df2fd38ca38c1923266d9f9
Denys Faber <Denys_Faber@epam.com>
2020-01-09 10:38:28 +0200

    changed game

--- CodingDojo/build-server.bat
+++ CodingDojo/build-server.bat
@@ -1,24 +1,24 @@
 set ROOT=%CD%
 
-set GAMES_TO_RUN=tetris,snake,bomberman
+set GAMES_TO_RUN=icancode
 
 IF "%GAMES_TO_RUN%"=="" (
     call %ROOT%\mvnw clean install -DskipTests
-    
+
     cd %ROOT%\server
     call %ROOT%\mvnw clean package -DskipTests -DallGames
 ) else (
     cd %ROOT%\games
     call %ROOT%\mvnw clean install -N
-    
+
     cd %ROOT%\games\engine
     call %ROOT%\mvnw clean install -DskipTests
-    
+
     for %%a in ("%GAMES_TO_RUN:,=" "%") do (
         cd %ROOT%\games\%%~a
         call %ROOT%\mvnw clean install -DskipTests
     )
-    
+
     cd %ROOT%\server
     call %ROOT%\mvnw clean package -DskipTests -P%GAMES_TO_RUN%
 )
